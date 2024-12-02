b878fb1743ad4ec3a93c884d9767f9d554c4badb
Ostap Hedz <lordanvil18@gmail.com>
2024-12-02 19:15:52 +0100

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,28 @@
+"use strict";
+// write your code here
+const body = document.querySelector("body");
+const logo = document.querySelector(".logo");
+const successCallBack = ()=>{
+    const divSuccess = document.createElement("div");
+    divSuccess.classList.add("message");
+    divSuccess.innerText = "Promise was resolved!";
+    body.append(divSuccess);
+};
+const errorCallBack = ()=>{
+    const divError = document.createElement("div");
+    divError.classList.add("message error-message");
+    divError.innerText = "Promise was rejected";
+    body.append(divError);
+};
+const promise1 = new Promise((resolve)=>{
+    logo.addEventListener("click", ()=>{
+        resolve();
+    });
+});
+const promise2 = new Promise((resolve, reject)=>{
+    setTimeout(()=>reject(new Error("rejected in 3 seconds")), 3000);
+});
+promise1.then(successCallBack).catch(errorCallBack);
+promise2.then(successCallBack).catch(errorCallBack);
+
+//# sourceMappingURL=index.f75de5e1.js.map
